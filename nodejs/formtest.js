24fc39276e249a814da2cf774a753eeb6a08bde4
Lakshminarayan Srinivasan <laksri@us.ibm.com>
2017-05-03 14:53:57 -0400

    Form

---
+++ nodejs/formtest.js
@@ -0,0 +1,41 @@
+var parameters = {
+  "first name": "John",
+  "last name": "Doe",
+  "zip": "27517"
+}
+
+function main(params) {
+  if (params.__ow_method == "get") {
+    var inputs = "";
+    for (i in parameters) {
+      inputs += "<input name='" + i + "' placeholder='" + i + "' value='" + parameters[i] + "' style='width:100%; padding:10px'><br/>"
+    }
+    
+    return {
+      html: "<html><head>\
+        <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>\
+        <script>$(function() {\n\
+            var url = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);\n\
+            url = url.substr(0, url.indexOf('.')) + '.json';\n\
+            $('#submit').click(function() {\n\
+                var data = {};\n\
+                $('input').each(function(i, el) { data[$(this).attr('name')] = $(this).val(); })\n\
+                $.ajax({ url, data, method: 'POST' })\n\
+                .done(function(data) { $('#result').val(JSON.stringify(data,null,2)); });\n\
+            })\n\
+        })\n\
+        </script></head><body>" + inputs + "<button id='submit' style='width: 100%; padding: 10px;'>Submit</button>\
+        <textarea id='result' style='width: 100%; height: 500px'></textarea></body></html>"
+    }
+  }
+    
+  var out = {};
+  for (var i in parameters) {
+      out[i] = params[i];
+  }
+  return { out };
+}
+
+if (require.main === module) console.log(JSON.stringify(main(parameters), null, 2));
+
+exports.main = main;
