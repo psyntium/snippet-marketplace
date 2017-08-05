9df191804e57c1703290ad38ce583f9030069c5e
snippet-java <snippet.java@gmail.com>
2017-08-05 11:46:10 +0800

    update testparams

--- nodejs/language-translation.js
+++ nodejs/language-translation.js
@@ -1,34 +1,51 @@
+const testparams = {
+  "textToTranslate": "That that is not confusing is amazing.",
+  "username": "",
+  "password": "",
+  "url": "https://sandbox-watson-proxy.mybluemix.net/language-translator/api",
+  "use_unauthenticated" : true
+}
+
 function main(params) {
-  const LanguageTranslationV2 =
-    require('watson-developer-cloud/language-translation/v2');
+  const LanguageTranslationV2 = require('watson-developer-cloud/language-translation/v2');
 
   var url = params.url || 'https://gateway.watsonplatform.net/language-translator/api' ;
   var use_unauthenticated =  params.use_unauthenticated || false ;
   
-  const language_translator = new LanguageTranslationV2({
-    'username': params.username,
-    'password': params.password,
-    'url' : url,
-    'use_unauthenticated': use_unauthenticated
-  });
+  var opts = {
+    "url": params.url || "https://gateway.watsonplatform.net/language-translator/api",
+    "use_unauthenticated": isTrue(use_unauthenticated)
+  }
+  
+  if (params.username && params.password) {
+    opts.username = params.username;
+    opts.password = params.password;
+  }
+  
+  var language_translator = new LanguageTranslationV2(opts);
 
   return new Promise(function (resolve, reject) {
     englishToFrench(params.textToTranslate, language_translator)
-      .then((results) => frenchToSpanish(results.translations[0].translation,
-                                         language_translator))
-      .then((results) => spanishToEnglish(results.translations[0].translation,
-                                          language_translator))
+      .then((results) => frenchToSpanish(results.translations[0].translation, language_translator))
+      .then((results) => spanishToEnglish(results.translations[0].translation, language_translator))
       .then((results) => resolve(results))
       .catch((err) => reject(err));
   });
 }
 
+function isTrue(value) {
+  if (value == "true" || value == true)
+    return true;
+  return false;
+}
+
 function englishToFrench(textToTranslate, language_translator) {
   return new Promise(function (resolve, reject) {
-    language_translator.
-      translate({'text': textToTranslate,
-                 'source': 'en', 'target': 'fr'},
-                function(err, res) {
+    language_translator.translate({
+        "text": textToTranslate,
+        "source": "en", 
+        "target": "fr"
+      }, function(err, res) {
       if (err)
         reject(err);
       else
@@ -39,10 +56,11 @@ function englishToFrench(textToTranslate, language_translator) {
 
 function frenchToSpanish(textToTranslate, language_translator) {
   return new Promise(function (resolve, reject) {
-    language_translator.
-      translate({'text': textToTranslate,
-                 'source': 'fr', 'target': 'es'},
-                function(err, res) {
+    language_translator.translate({
+        "text": textToTranslate,
+        "source": "fr", 
+        "target": "es"
+      }, function(err, res) {
       if (err)
         reject(err);
       else
@@ -53,10 +71,11 @@ function frenchToSpanish(textToTranslate, language_translator) {
 
 function spanishToEnglish(textToTranslate, language_translator) {
   return new Promise(function (resolve, reject) {
-    language_translator.
-      translate({'text': textToTranslate,
-                 'source': 'es', 'target': 'en'},
-                function(err, res) {
+    language_translator.translate({
+        "text": textToTranslate,
+        "source": "es", 
+        "target": "en"
+      }, function(err, res) {
       if (err)
         reject(err);
       else
@@ -65,15 +84,9 @@ function spanishToEnglish(textToTranslate, language_translator) {
   });
 }
 
-const defaultParameters = {
-  'textToTranslate' : 'That that is not confusing is amazing.',
-  'username'        : '',
-  'password'        : '',
-  'url'             : 'https://sandbox-watson-proxy.mybluemix.net/language-translator/api',
-  'use_unauthenticated' : true
-}
-
 if (require.main === module)
-  main(defaultParameters)
+  main(testparams)
     .then((results) => console.log(results.translations[0].translation))
     .catch((error) => console.log(error.message));
+
+exports.main = main;
