e23f61a11c1690f6b6c5cea5da36a10b722d746f
snippet-java <snippet-java@gmail.com>
2017-11-27 17:53:33 +0800

    Google crud samples

---
+++ nodejs/Google/create-file-drive.js
@@ -0,0 +1,69 @@
+var request = require('request');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+var testparams = {
+  "drive_oauth_client_secret": "",
+  "drive_oauth_client_id": "",
+  "drive_oauth_redirect_url": ["urn:ietf:wg:oauth:2.0:oob"],
+  "token": {
+    "refresh_token": ""
+  }
+};
+
+function main(params) {
+
+  return new Promise(function(resolve, reject){
+      console.log(params);
+      var content = {
+        "client_secret": params.drive_oauth_client_secret,
+        "client_id": params.drive_oauth_client_id,
+        "redirect_uris": params.drive_oauth_redirect_url
+      }
+      authorize(content, uploadFile);
+      /**
+       * Create an OAuth2 client with the given credentials, and then execute the
+       * given callback function.
+       *
+       * @param {Object} credentials The authorization client credentials.
+       * @param {function} callback The callback to call with the authorized client.
+       */
+      function authorize(credentials, callback) {
+        var clientSecret = credentials.client_secret;
+        var clientId = credentials.client_id;
+        var redirectUrl = credentials.redirect_uris[0];
+        var auth = new googleAuth();
+        var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+        oauth2Client.credentials = params.token;
+        callback(oauth2Client);
+      }
+
+      function uploadFile(auth) {
+        var service = google.drive('v3');
+        service.files.create({
+          auth: auth,
+          resource: {
+            name: 'Test2',
+            mimeType: 'text/plain'
+          },
+          media: {
+            mimeType: 'text/plain',
+            body: 'Uploaded another text from Sandbox'
+          }
+        }, function(err, response) {
+          if (err)
+            return reject({err});
+
+          return resolve({response});
+        });
+      }
+  })
+}
+
+if (require.main === module)
+  main(testparams)
+    .then((results) => console.log(results))
+    .catch((error) => console.log(error));
+
+exports.main = main;
---
+++ nodejs/Google/get-file-drive.js
@@ -0,0 +1,61 @@
+var request = require('request');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var fs = require('fs');
+
+var testparams = {
+  "drive_oauth_client_secret": "",
+  "drive_oauth_client_id": "",
+  "drive_oauth_redirect_url": ["urn:ietf:wg:oauth:2.0:oob"],
+  "token": {
+    "refresh_token": ""
+  },
+  "fileId": ""
+};
+
+function main(params) {
+
+  return new Promise(function(resolve, reject){
+      var content = {
+        "client_secret": params.drive_oauth_client_secret,
+        "client_id": params.drive_oauth_client_id,
+        "redirect_uris": params.drive_oauth_redirect_url
+      }
+      authorize(content, getFile);
+      /**
+       * Create an OAuth2 client with the given credentials, and then execute the
+       * given callback function.
+       *
+       * @param {Object} credentials The authorization client credentials.
+       * @param {function} callback The callback to call with the authorized client.
+       */
+      function authorize(credentials, callback) {
+        var clientSecret = credentials.client_secret;
+        var clientId = credentials.client_id;
+        var redirectUrl = credentials.redirect_uris[0];
+        var auth = new googleAuth();
+        var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+        oauth2Client.credentials = params.token;
+        callback(oauth2Client);
+      }
+
+      function getFile(auth) {
+          var service = google.drive('v3');
+          service.files.get({
+            auth: auth,
+            fileId: params.fileId
+          }, function(err, response){
+            if(err) return reject({err});
+            else return resolve({response});
+          })
+      }
+  })
+}
+
+if (require.main === module)
+  main(testparams)
+    .then((results) => console.log(results))
+    .catch((error) => console.log(error));
+
+exports.main = main;
